dc0629625b37b36756bb724ad6b83cdba42c88a3
Paul1ta <128856339+Paul1ta@users.noreply.github.com>
2023-10-21 17:53:53 -0300

    Add files via upload

---
+++ tp5/c_escudos.js
@@ -0,0 +1,20 @@
+class Escudo {       
+  constructor(x, y) {     // Constructor de clase.
+    this.x = x;           // Se   asignan  valores 
+    this.y = y+50;        // utilizados  dentro de
+    this.tamano = 180;    // la   clase  'escudo'.
+  }
+ 
+  mover() {                   // Controla el movimiento
+    this.x += random(1, 3);   // de los 'escudos' en el
+    if (this.x > width) {     // trayecto   del   juego.
+     this.x = random(-200,-120);
+      this.y = random(height/4);
+    }
+  }
+
+  mostrar() {                // Se define cómo y dónde 
+    fill(0, 0, 255);         // se muestra  el  escudo.
+    image(escudo, this.x, this.y, this.tamano, this.tamano);
+  }
+}
